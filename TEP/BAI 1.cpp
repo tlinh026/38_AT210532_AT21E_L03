05223978ec825b8b1838140538395729056187cf
ThuyLinh <linhnguyenthuy614@gmail.com>
2025-04-14 02:27:45 +0700

    bt ltcb

--- TEP/BAI 1.cpp
+++ TEP/BAI 1.cpp
@@ -1,49 +1,40 @@
 #include <stdio.h>
 
-void nhapMaTran(int m, int n, int mat[][n], char ten) {
-	printf("Nhap ma tran %c co kich thuoc %dx%d:\n", ten, m, n);
-	for (int i=0; i < m; i++){
-		for (int j = 0; j < n; j++) {
-			printf("%c[%d][%d]; ", ten, i, j);
-			scanf("%d", &mat[i][j]);
-		}
-	}
-}
-void ghiMaTran(const char *tenTep, int m, int n, int mat[][n]) {
-	FILE *file = fopen(tenTep, "w");
-	if (file == NULL) {
-		printf("Khong the mo tep!\n");
-		return;
-	}
-	fprinf(file, "Ma tran ket qua:\n ");
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			fprintf(file, "%d", mat[i][j]);
-		}
-		fprintf(file, "\n");
-	}
-	fclose(file);
-	printf("Da ghi ma tran vao tep %s\n", tenTep);
-}
-void tongMaTran(int m, int n, int matA[][n], int matB[][n], int matC[][n]) {
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			matC[i][j] = matA[i][j] + matB[i][j];
-		}
-	}
-}
+#define MAX 100
+
 int main() {
-	int m, n;
-	printf("Nhap kich thuoc ma tran (m,n): ");
-	scanf("%d %d", &m, &n);
-	
-	int matA[m][n], matB[m][n], matC[m][n];
-	nhapMaTran(m, n, matA, 'A');
-	nhapMaTran(m, n, matB, 'B');
-	
-	tongMaTran(m, n, matA, matB, matC);
-	
-	ghiMaTran("CONG_MT.C", m, n, matC);
-	
-	return 0;
+    int A[MAX][MAX], B[MAX][MAX], C[MAX][MAX];
+    int m, n;
+    FILE *fp;
+
+    printf("Nhap so dong va cot cua ma tran: ");
+    scanf("%d%d", &m, &n);
+
+    printf("Nhap ma tran A:\n");
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Nhap ma tran B:\n");
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &B[i][j]);
+
+    // Tinh tong
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+
+    // Ghi vào file
+    fp = fopen("CONG_MT.C", "w");
+    fprintf(fp, "Ma tran tong C:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            fprintf(fp, "%5d", C[i][j]);
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+    printf("Da ghi ket qua vao file CONG_MT.C\n");
+    return 0;
 }
+
